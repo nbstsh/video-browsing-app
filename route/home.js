74b3351cda8d36e8649b15205d4a41829cb95a20
nbstsh <nbstsh@gmail.com>
2019-02-15 21:30:15 +0900

    sort selected schedules by current day

--- route/home.js
+++ route/home.js
@@ -1,12 +1,39 @@
+const config = require('config')
+const moment = require('moment')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    const schedules = await getSelectedSchedules()
-    const times = generateTimelist(config.get('schedule.duration'))
+const { findSelectedSchedule } = require('../models/helper')
 
-    res.render('index', { week: schedules.week, times })
+router.get('/', async (req, res) => {
+    const { days } = await findSelectedSchedule()
+    if (!days) {
+        //TODO: error handling
+        // default の selectedScheduleを用意しておく必要があるかもしれない
+        return res.status(404).send('error: selected schedule not found')
+    }
+
+    const times = days[0].times.reduce((acc, cur) =>  {
+        acc.push(cur.time)
+        return acc
+    }, [])
+
+
+    sortByCurrentDay(days)
+
+    res.render('index', { days, times })
 })
+
+
+function sortByCurrentDay(days) {
+    const currentDay = moment().day()
+    const add = moment().day('saturday').day() + 1
+    const adjustNum = (day) => {
+        const num = moment().day(day).day()
+        return num < currentDay ? num + add : num
+    } 
+    days.sort((a, b) => adjustNum(a.day) - adjustNum(b.day))
+}
 
 
 module.exports = router
